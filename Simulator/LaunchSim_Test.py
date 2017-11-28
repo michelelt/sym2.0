ad271b701dce62e4a9598c731b94959ceb2318f5
Danilo Giordano <danilo.giordano@polito.it>
2017-11-28 13:51:27 +0100

    modify directory structure

---
+++ Simulator/LaunchSim_Test.py
@@ -0,0 +1,65 @@
+import sys
+import os
+p = os.path.abspath('..')
+sys.path.append(p+"/")
+
+from Simulator.RunSim import *
+from Simulator.Globals.GlobalVar import * 
+import datetime as datetime
+import pickle
+
+from multiprocessing import Process
+import multiprocessing
+
+def main():
+
+
+    walkingTreshold = 1000000#int(sys.argv[4]) # in [m]
+
+    zoneEnjoy = 220
+
+    # countNoRech = {}
+
+    #BookingID_Car = load()
+    a = datetime.datetime.now()
+    Stamps_Events = pickle.load( open( "../events/"+provider+"_sorted_dict_events_obj.pkl", "rb" ) )
+    b = datetime.datetime.now()    
+    c = (b - a).total_seconds()
+    print("End Load Events: "+str(int(c)))
+
+
+    a = datetime.datetime.now()    
+    global DistancesFrom_Zone_Ordered 
+    DistancesFrom_Zone_Ordered = pickle.load( open( "../input/"+provider+"_ZoneDistances.p", "rb" ) )
+    b = datetime.datetime.now()    
+    c = (b - a).total_seconds()
+    print("End Load Zones: "+str(int(c)))
+    i=0
+    
+    ZoneCars = pickle.load( open( "../input/"+provider+"_ZoneCars.p", "rb" ) )
+
+
+    numberOfStations = 160#len(RechargingStation_Zones)#int(sys.argv[1])
+    algorithm = "rnd"#str(sys.argv[2])
+    
+    a = datetime.datetime.now()    
+    global RechargingStation_Zones
+    RechargingStation_Zones = loadRecharing(algorithm, provider, numberOfStations)
+    b = datetime.datetime.now()    
+    c = (b - a).total_seconds()
+    print("End Load Recharging: "+str(int(c)))
+
+    print(RechargingStation_Zones)
+    #simulation 1
+    
+    tankThreshold = 50#int(sys.argv[3]) # in [%]
+
+    
+    return_dict = {}
+    RunSim(algorithm,numberOfStations,tankThreshold,walkingTreshold,ZoneCars,Stamps_Events,\
+       RechargingStation_Zones,DistancesFrom_Zone_Ordered,return_dict,-1,-1)
+                                               
+    return
+
+main()
+
