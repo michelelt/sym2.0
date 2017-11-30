e49e9cd93e9fdd931371eee74becb0f680d00f79
Danilo Giordano <danilo.giordano@polito.it>
2017-11-30 16:49:21 +0100

    change init

--- InitializeFoldersandInputs.sh
+++ InitializeFoldersandInputs.sh
@@ -4,8 +4,10 @@ mkdir input
 mkdir output
 mkdir events
 
-python3 InputCreation/CreateCarInitDataset.py
-python3 InputCreation/CreatePickleEventi.py 
-python3 InputCreation/CreatePickleZoneDistance.py
-python3 InputCreation/CreateValidZones.py
+cd InputCreation
+
+python3 CreateCarInitDataset.py
+python3 CreatePickleEventi.py 
+python3 CreatePickleZoneDistance.py
+python3 CreateValidZones.py
 
